819c7f168a4d01fb8f2d60de05dde3bf805e24ab
nayemshanto333 <nayemshantobd@gamil.com>
2025-05-21 00:38:42 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-center font-bold bg-gray-400 text-4xl'>Hey Creative People</h1>
+    </>
+  )
+}
+
+export default App
